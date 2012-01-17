faae8afcd1b74731e304ea7920ee0e39fe6bebb3
code1n <lomov.as@gmail.com>
2012-01-17 02:35:53 +0200

    rotate around X axis

--- rotate3Di.js
+++ rotate3Di.js
@@ -3,15 +3,15 @@
     // "3D" isometric rotation and animation using CSS3 transformations
     // currently supported in Safari/Chrome/Webkit, Firefox 3.5+, IE 9+,
     // and Opera 11+. Tested with jQuery 1.3.x through 1.6.
-    
-    
+
+
     var calcRotate3Di = {
         direction: function (now) {return (now < 0 ? -1 : 1);},
         degrees: function (now) {return (Math.floor(Math.abs(now))) % 360;},
         scale: function (degrees) {return (1 - (degrees % 180) / 90)
                                             * (degrees >= 180 ? -1 : 1);}
     }
-    
+
     // Custom animator
     $.fx.step.rotate3Di = function (fx) {
         direction = calcRotate3Di.direction(fx.now);
@@ -21,7 +21,7 @@
         if (fx.options && typeof fx.options['sideChange'] != 'undefined') {
             if (fx.options['sideChange']) {
                 var prevScale = $(fx.elem).data('rotate3Di.prevScale');
-                
+
                 // negative scale means back side
                 // positive scale means front side
                 // if one is pos and one is neg then we have changed sides
@@ -37,7 +37,7 @@
                     // once, but then that broke legitimate need to call it
                     // more than once for rotations of 270+ degrees!
                     //fx.options['sideChange'] = null;
-                    
+
                     // this is my answer to commenting the above thing out...
                     // if we just flipped sides, flip-flop the old previous
                     // scale so that we can fire the sideChange event correctly
@@ -62,11 +62,11 @@
         $(fx.elem).data('rotate3Di.degrees', direction * degrees);
         $(fx.elem).css(
             'transform',
-            'skew(0deg, ' + direction * degrees + 'deg)'
-                + ' scale(' + scale + ', 1)'
+            'skew(' + direction * degrees + 'deg, 0deg)'
+                + ' scale(1, ' + scale + ')'
         );
     }
-    
+
     // fx.cur() must be monkey patched because otherwise it would always
     // return 0 for current rotate3Di value
     var proxied = $.fx.prototype.cur;
@@ -82,33 +82,33 @@
                 }
             }
         }
-        
+
         return proxied.apply(this, arguments);
     }
-    
+
     $.fn.rotate3Di = function (degrees, duration, options) {
         if (typeof duration == 'undefined') {
             duration = 0;
         }
-        
+
         if (typeof options == 'object') {
             $.extend(options, {duration: duration});
         } else {
             options = {duration: duration};
         }
-        
+
         if (degrees == 'toggle') {
             // Yes, jQuery has the toggle() event but that's only good for
             // clicks, and likewise hover() is only good for mouse in/out.
             // What if you want to toggle based on a timer or something else?
             if ($(this).data('rotate3Di.flipped')) {
                 degrees = 'unflip';
-                
+
             } else {
                 degrees = 'flip';
             }
         }
-        
+
         if (degrees == 'flip') {
             $(this).data('rotate3Di.flipped', true);
 
@@ -120,15 +120,15 @@
             ) {
                 direction = 1;
             }
-            
+
             degrees = direction * 180;
-            
+
         } else if (degrees == 'unflip') {
             $(this).data('rotate3Di.flipped', false);
-            
+
             degrees = 0;
         }
-        
+
         var d = $(this).data('rotate3Di.degrees') || 0;
         $(this).data(
             'rotate3Di.prevScale',
